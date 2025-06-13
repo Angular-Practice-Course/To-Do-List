705b15651a9058f2fb02fd90bfd410b4dd5491e3
Gonzalo Pinell <gonzalopinell@gmail.com>
2025-06-13 15:49:25 -0400

    Refactor project structure and integrate ESLint with Tailwind CSS support. Updated Angular configuration, added linting options, and modified styles. Removed unused app files and improved code formatting across various components.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected title = 'todo-list';
+
+  @Input('test') count = 0;
 }
